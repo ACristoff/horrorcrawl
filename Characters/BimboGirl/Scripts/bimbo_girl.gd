5f5765b787cfe6d8ede3434af51adb4ff8267d4c
kinteyoi <59457560+kinteyoi@users.noreply.github.com>
2025-05-31 00:57:47 -0500

    BimboGirlComesAlive

--- Characters/BimboGirl/Scripts/bimbo_girl.gd
+++ Characters/BimboGirl/Scripts/bimbo_girl.gd
@@ -1,16 +1,31 @@
 extends CharacterBody2D
 
-var speed = 50
+var speed = 80
 @onready var body = $Body
 @onready var movement = $Movement
+@onready var arms = $Arms
+@onready var silenced_pistol_held = $Body/SilencedPistolHeld
+@onready var step_sfx = preload("res://Characters/BimboGirl/Assets/sfx_step_sand_l.mp3")
 
+var is_armed = false
+func armed():
+	is_armed = true
+	silenced_pistol_held.visible = true
+	
+func step():
+	AudMan.play_sfx(step_sfx)
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	if velocity:
 		movement.play("Walk")
+		if is_armed:
+			arms.play("ArmedWalk")
+		else:
+			arms.play("UnarmedWalk")
 	else:
 		movement.play("Idle")
+		arms.play("Idle")
 	velocity = direction * speed
 	body.look_at(get_global_mouse_position())
 	body.rotation_degrees -= 90
